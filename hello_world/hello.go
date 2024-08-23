88d4290398c2eaf7ca0c92b4f3f0f4dfb69eb8b8
Ivan Toriya <ivantoriya@gmail.com>
2024-08-23 14:29:57 +0200

    add languages

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,16 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+	russian = "Russian"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	russianHelloPrefix = "Здравствуйте, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world!"))
+	fmt.Println(Hello("world!", ""))
 }
